282def6bce612399ee6a3dc3928267025a4a80f6
Ruukaas <lucashenriquehca@gmail.com>
2024-09-04 01:08:48 -0300

    feat: add user registration functionality

--- src/banco.py
+++ src/banco.py
@@ -1,8 +1,12 @@
+from datetime import datetime
+
+
 def menu():
         menu = """Escolha a operação desejada:
 [1] Depositar
 [2] Sacar
 [3] Extrato
+[4] Cadastrar novo usuário
 [0] Sair
 -->"""
         return int(input(menu))
@@ -41,7 +45,7 @@ def sacar(*,saldo, valor, extrato, limite, numero_saques, limite_saques):
                 print("Operação não realizada. Insira um valor válido\n")
                 return None, None, None
         
-def exibir_extrato(saldo,/,*,extrato):
+def exibir_extrato(saldo,usuarios,/,*,extrato):
         if(len(extrato) == 0):
                 print("Não foram realizadas movimentações")
         else:
@@ -50,8 +54,73 @@ def exibir_extrato(saldo,/,*,extrato):
                         print(f"Entrada {indice+1}: {entrada}")      
                 print(f"\nSaldo atual: R$ {saldo:.2f}")
                 print("######################################")
-                print() 
+                print()
                 
+def filtrar_usuario(cpf, usuarios):
+        usuarios_filtrados = [usuario for usuario in usuarios if usuario["cpf"] == cpf]
+        return usuarios_filtrados[0] if usuarios_filtrados else None
+
+def validar_cpf(cpf):
+    if(len(cpf) == 11 and cpf.isdigit()):
+        return True
+    else:
+        return False
+
+def validar_data_formato_br(data):
+        try:
+                datetime.strptime(data, '%d-%m-%Y')
+                return True
+        except ValueError:
+                return False
+        
+def validar_endereço(endereço):
+        try:
+                rua, resto = endereço.split(',', 1) 
+                numero, resto = resto.split(' - ', 1)
+                bairro, resto = resto.split(' - ', 1)
+                cidade, estado = resto.split('/', 1)
+        
+                if (rua.strip() and numero.strip().isdigit() and bairro.strip() and cidade.strip() and len(estado.strip()) == 2 and estado.isupper()):
+                        return True
+                else:
+                        return False
+        except ValueError:
+                return False
+def criar_entrada_usuario(cpf):
+        nome = input("Informe o nome completo: ")
+        
+        data_nascimento = input("Informe a data de nascimento (Exatamente no seguinte formato: dd-mm-aaaa): ")
+        data_valida = validar_data_formato_br(data_nascimento)
+        if(not data_valida):
+                print("Operação não realizada. Data inserida em um formato errado")
+                return
+        
+        endereco = input("Informe o endereço (Exatamente no seguinte formato: logradouro, numero - bairro - cidade/sigla estado): ")
+        endereco_valido = validar_endereço(endereco)
+        if(not endereco_valido):
+                print("Operação não realizada. Endereço inserido em um formato errado.")
+                return
+                
+        return {"nome": nome, "data_nascimento": data_nascimento, "cpf": cpf, "endereco": endereco}
+
+def criar_usuario(usuarios):
+        cpf = input("Informe o CPF para o cadastro(somente números, 11 digitos): ")
+        cpf_valido = validar_cpf(cpf)
+        if(not cpf_valido):
+                print("Operação não realizada. CPF inválido")
+                return
+        
+        usuario = filtrar_usuario(cpf, usuarios)
+        
+        if(usuario): #Se já existir um usuário com esse CPF cadastrado
+                print("Operação não realizada. CPF já cadastrado.")
+                return 
+        
+        usuario = criar_entrada_usuario(cpf)
+        if(usuario): #Se ele conseguiu criar o usuário com sucesso
+                usuarios.append(usuario)
+                print(f"Usuário {usuario["nome"]} com o CPF {cpf} foi criado com sucesso.\n")
+      
 def main():
         boas_vindas = "Bem vindo ao seu sistema bancário!"
         
@@ -60,6 +129,7 @@ def main():
         extrato = []
         numero_saques = 0
         LIMITE_SAQUES = 3
+        usuarios = []
         
         print(boas_vindas)
         
@@ -85,7 +155,9 @@ def main():
                                 extrato = extrato_atual_saque
                                 numero_saques = numero_saques_atual_saque
                 elif(opcao == 3):
-                        exibir_extrato(saldo, extrato = extrato)
+                        exibir_extrato(saldo,usuarios, extrato = extrato)
+                elif(opcao == 4):
+                        criar_usuario(usuarios)
                 elif(opcao == 0):
                         print("Até logo.")
                         break
