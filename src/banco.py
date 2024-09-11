30a9ae457bce8f1ca022e029f00d21181ce7d4b9
Ruukaas <lucashenriquehca@gmail.com>
2024-09-11 20:07:31 -0300

    feat: create client class

--- src/banco.py
+++ src/banco.py
@@ -1,5 +1,16 @@
 from datetime import datetime
 
+class Cliente():
+        def __init__(self, endereco):
+                self.endereco = endereco
+                self.contas = []
+                
+        def realizar_transacao(self,conta, transacao):
+                transacao.registrar(conta)
+        
+        def adicionar_conta(self, conta):
+                self.contas.append(conta)
+
 
 def menu():
         menu = """Escolha a operação desejada:
